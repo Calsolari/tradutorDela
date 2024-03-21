194fd1fc72c9f747e80d58ee487820c9a759d65b
natybastazini <natybastazini7@gmail.com>
2024-03-21 12:13:15 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 'use strict'
 
 const botaoTema = document.getElementById('button-theme')
+const body = document.getElementById('body')
 
 function mudarTema() {
     
-    botaoTema.classList.toggle('temaEscuro')
-    
-    
+    if(botaoTema.children[0].src.includes('sol')){
+        botaoTema.children[0].src = './img/lua.png'
+        botaoTema.children[0].alt = 'Lua'
+    } else {
+        botaoTema.children[0].src = './img/sol.png'
+        botaoTema.children[0].alt = 'Sol'
+    }
+    botaoTema.classList.toggle('tema-escuro')
+    body.classList.toggle('tema-escuro')
 }
 
 botaoTema.addEventListener('click', mudarTema)
