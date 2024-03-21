19a9f9e402e8efd5f2e3f0de6b87d8f83f2375b2
natybastazini <natybastazini7@gmail.com>
2024-03-21 11:57:45 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 'use strict'
 
-const botaoTema = document.getElementById('button')
+const botaoTema = document.getElementById('button-theme')
 
 function mudarTema() {
     
     botaoTema.classList.toggle('temaEscuro')
-
-
+    
+    
 }
 
 botaoTema.addEventListener('click', mudarTema)
-
